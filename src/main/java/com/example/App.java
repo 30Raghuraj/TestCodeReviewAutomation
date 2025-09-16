c52a16f847caa6ddc4c6cebff09ec86a85b015ab
Raghu Singh <raghu.singh@siemens.com>
2025-09-16 16:03:06 +0530

    'testbugPR'

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
+	//now str is null
         String str = null;
         if (str.equals("hello")) {  // SpotBugs will flag possible NullPointerException
             System.out.println("Hello");
